1e9e4e5fc3756973893371c818e44b6b146579f3
Taha Muneeb <tahamuneeb2@gmail.com>
2025-03-20 02:10:03 +0400

    project init

---
+++ Feedly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Feedly
+//
+//  Created by Taha Muneeb on 20/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Feedly/FeedlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedlyApp.swift
+//  Feedly
+//
+//  Created by Taha Muneeb on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FeedlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
