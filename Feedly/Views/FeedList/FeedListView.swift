583b3d67ae3b91d4cde1c04c759b725be1dd0dae
Taha Muneeb <tahamuneeb2@gmail.com>
2025-03-20 03:13:04 +0400

    Created FeedlistView

---
+++ Feedly/Views/FeedList/FeedListView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedListView.swift
+//  Feedly
+//
+//  Created by Taha Muneeb on 20/03/2025.
+//
+
+import SwiftUI
+
+struct FeedListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedListView()
+}
