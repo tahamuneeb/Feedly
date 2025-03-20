98661353ba9c8f5527d7bd0c14314e127b2901a7
Taha Muneeb <tahamuneeb2@gmail.com>
2025-03-20 20:11:20 +0400

    create scheme for Feedly Core tests

---
+++ FeedlyCoreTests/API/URLSessionHttpClientTests.swift
@@ -0,0 +1,15 @@
+//
+//  URLSessionHttpClientTests.swift
+//  Feedly
+//
+//  Created by Taha Muneeb on 20/03/2025.
+//
+
+import XCTest
+import FeedlyCore
+
+final class URLSessionHttpClientTests: XCTestCase {
+    func test_test() {
+        XCTAssertTrue(true)
+    }
+}
