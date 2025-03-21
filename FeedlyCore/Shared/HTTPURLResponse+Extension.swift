2872f3ebb26967c62c94e5f5e4ee74bdb2bea41e
Taha Muneeb <tahamuneeb2@gmail.com>
2025-03-21 20:50:22 +0400

    test cases for the mapping of `FeedDataMapper`

--- FeedlyCore/Shared/HTTPURLResponse+Extension.swift
+++ FeedlyCore/Shared/HTTPURLResponse+Extension.swift
@@ -7,9 +7,7 @@
 import Foundation
 
 extension HTTPURLResponse {
-	private static var OK_200: Int { return 200 }
-
 	var isOK: Bool {
-		return statusCode == HTTPURLResponse.OK_200
+        return statusCode >= 200 && statusCode <= 299
 	}
 }
--- FeedlyCoreTests/Feed/FeedDataMapperTests.swift
+++ FeedlyCoreTests/Feed/FeedDataMapperTests.swift
@@ -22,6 +22,59 @@ final class FeedDataMapperTests: XCTestCase {
         
     }
     
+    func test_map_throwsErrorOnInvalidJSON() {
+        let invalidJSON = Data("invalid json".utf8)
+        
+        XCTAssertThrowsError(
+            try FeedDataMapper.map(invalidJSON, response: HTTPURLResponse(statusCode: 200))
+        )
+    }
+    
+    func test_map_deliversEmptyOnEmptyJSONList() throws {
+        let emptyListJSON = makeJSON([])
+        
+        let feeds = try FeedDataMapper.map(emptyListJSON, response: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(feeds, [])
+    }
+    
+    func test_map_deliversItemsOnJSONList() throws {
+        let feed1 = makeFeedModel(
+            id: 1,
+            url: URL(string: "https://a-url.com")!,
+            width: 100,
+            height: 100,
+            creatorName: "a creator",
+            thumbnailUrl: nil,
+            mediaType: .image
+        )
+        
+        let feed2 = makeFeedModel(
+            id: 2,
+            url: URL(string: "https://another-url.com")!,
+            width: 200,
+            height: 200,
+            creatorName: "another creator",
+            thumbnailUrl: URL(string: "https://another-url-thumb.com")!,
+            mediaType: .video
+        )
+        
+//        let feeds = [feed1.model, feed2.model]
+        let json = makeJSON([feed1.json, feed2.json])
+        
+//        let receivedFeeds = try FeedDataMapper.map(json, response: HTTPURLResponse(statusCode: 200))
+        
+        let statusCodes = [200, 201, 205, 299]
+
+        try statusCodes.forEach { code in
+            let result = try FeedDataMapper.map(json, response: HTTPURLResponse(statusCode: code))
+            XCTAssertEqual(result, [feed1.model, feed2.model])
+        }
+        
+        // XCTAssertEqual(receivedFeeds, feeds)
+    }
+        
+    
     // MARK: - Helpers
     func makeFeedModel(
         id: Int,
@@ -42,15 +95,25 @@ final class FeedDataMapperTests: XCTestCase {
             mediaType: mediaType
         )
         
-        let json: [String: Any?] = [
+        var json: [String: Any?] = [
             "id": model.id,
-            "url": model.url.absoluteString,
             "width": model.width,
             "height": model.height,
-            "creatorName": model.creatorName,
-            "thumbnailUrl": model.thumbnailUrl?.absoluteString,
-            "mediaType": mediaType.rawValue
+            "type": mediaType.rawValue
         ]
+        
+        if mediaType == .image {
+            json["src"] = [
+                "large": model.url.absoluteString
+            ]
+            json["photographer"] = model.creatorName
+        } else if mediaType == .video {
+            json["video_files"] = [
+                ["quality": "sd", "link": model.url.absoluteString]
+            ]
+            json["user"] = ["name": model.creatorName]
+            json["image"] = model.thumbnailUrl?.absoluteString
+        }
         
         return (model, json)
     }
